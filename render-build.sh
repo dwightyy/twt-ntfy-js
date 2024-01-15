3f580ba80c381e8f6ea9e0b95649236a49266836
jp <jp@jps-MacBook-Air.local>
2024-01-15 14:09:33 -0300

    updating render script

--- render-build.sh
+++ render-build.sh
@@ -4,14 +4,14 @@ set -o errexit
 
 STORAGE_DIR=/opt/render/project/.render
 
-if [[ ! -d $STORAGE_DIR/chrome ]]; then
+if [ ! -d "$STORAGE_DIR/chrome" ]; then
   echo "...Downloading Chrome"
-  mkdir -p $STORAGE_DIR/chrome
-  cd $STORAGE_DIR/chrome
+  mkdir -p "$STORAGE_DIR/chrome"
+  cd "$STORAGE_DIR/chrome"
   wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-  dpkg -x ./google-chrome-stable_current_amd64.deb $STORAGE_DIR/chrome
+  dpkg -x ./google-chrome-stable_current_amd64.deb "$STORAGE_DIR/chrome"
   rm ./google-chrome-stable_current_amd64.deb
-  cd $HOME
+  cd "$HOME"
 else
   echo "...Using Chrome from cache"
 fi
