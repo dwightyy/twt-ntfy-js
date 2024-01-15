9cb8ad5ea2dd436f71d7f8004892e706a278fcdd
jp <jp@jps-MacBook-Air.local>
2024-01-15 14:57:35 -0300

    test

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ async function runScript() {
   const chromeOptions = new chrome.Options();
   chromeOptions.addArguments("--headless");
   chromeOptions.addArguments("--disable-gpu");
-  const user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36";
+  const user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.5735.346 Safari/537.36";
   chromeOptions.addArguments(`user-agent=${user_agent}`);
 
   const driver = await new Builder().forBrowser('chrome').setChromeOptions(chromeOptions).build();
